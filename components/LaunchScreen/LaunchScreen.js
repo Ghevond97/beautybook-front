4fcc159de0825bafa0140f870421ab6438908a22
Ghevond97 <ghevond.mirzakhanyan@gmail.com>
2019-02-09 16:17:14 +0400

    LauncScreen component created, react-native-linear-gradien configs set and working, SalonCard component inporcess

---
+++ components/LaunchScreen/LaunchScreen.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, Image } from 'react-native';
+
+import LinearGradient from 'react-native-linear-gradient';
+
+
+class LaunchScreen extends Component {
+  render() {
+    return (
+      <LinearGradient colors={['#9F83D8', '#1CF4F1']} style={styles.linearGradient}>
+      <Image 
+        source={require('../../assets/images/logo.png')}
+      />
+      </LinearGradient>
+    )
+  }
+}
+
+var styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  },
+});
+
+export default LaunchScreen;
---
+++ components/SalonCard/SectionCard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import { StyleSheet, View } from 'react-native'
+
+class SectionCard extends Component {
+  render() {
+    return(
+      <View style={StyleSheet.card}>
+
+      </View>      
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    backgroundColor: '#FFFFFF'
+  }
+})
